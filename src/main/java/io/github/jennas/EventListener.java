732f0d3a10186d73ea229fa8094bfb06e0fee9b6
Jennas-Lee <leezeunjun@gmail.com>
2022-08-09 13:47:21 +0900

    Add EventListener.java and GetEC2Instance.java.

---
+++ src/main/java/io/github/jennas/EventListener.java
@@ -0,0 +1,53 @@
+package io.github.jennas;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class EventListener implements ActionListener {
+    JTextField accessKeyTextField;
+    JTextField secretAccessKeyTextField;
+    JComboBox<String> regionComboBox;
+    JTable instanceTable;
+
+    EventListener(
+            JTextField accessKeyTextField,
+            JTextField secretAccessKeyTextField,
+            JComboBox<String> regionComboBox,
+            JTable instanceTable
+    ) {
+        this.accessKeyTextField = accessKeyTextField;
+        this.secretAccessKeyTextField = secretAccessKeyTextField;
+        this.regionComboBox = regionComboBox;
+        this.instanceTable = instanceTable;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String accessKey = this.accessKeyTextField.getText();
+        String secretAccessKey = this.secretAccessKeyTextField.getText();
+        String region = this.regionComboBox.getItemAt(this.regionComboBox.getSelectedIndex());
+
+        removeAllTableRows();
+        getEC2Instance(accessKey, secretAccessKey, region);
+    }
+
+    private void removeAllTableRows() {
+        DefaultTableModel instanceTableModel = (DefaultTableModel) this.instanceTable.getModel();
+
+        instanceTableModel.setRowCount(0);
+    }
+
+    private void getEC2Instance(String accessKey, String secretAccessKey, String region) {
+        GetEC2Instance ec2 = new GetEC2Instance(accessKey, secretAccessKey, region);
+        DefaultTableModel instanceTableModel = (DefaultTableModel) this.instanceTable.getModel();
+
+        ArrayList<String[]> ec2List = ec2.getEC2List();
+
+        for (String[] ec2Items : ec2List) {
+            instanceTableModel.addRow(ec2Items);
+        }
+    }
+}
--- src/main/java/io/github/jennas/Main.java
+++ src/main/java/io/github/jennas/Main.java
@@ -1,36 +1,94 @@
 package io.github.jennas;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 class Frame extends JFrame {
+    private final String[] region = {
+            "us-east-1",
+            "us-east-2",
+            "us-west-1",
+            "us-west-2",
+            "af-south-1",
+            "ap-east-1",
+            "ap-south-1",
+            "ap-northeast-1",
+            "ap-northeast-2",
+            "ap-northeast-3",
+            "ap-southeast-1",
+            "ap-southeast-2",
+            "ap-southeast-3",
+            "ca-central-1",
+            "eu-central-1",
+            "eu-west-1",
+            "eu-west-2",
+            "eu-west-3",
+            "eu-north-1",
+            "eu-south-1",
+            "me-south-1",
+            "sa-east-1"
+    };
+    JTable instanceTable;
+
     public Frame() {
         setTitle("EC2 List");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container cp = getContentPane();
         cp.setLayout(new BorderLayout());
 
+        JPanel tablePanel = new JPanel();
+        JPanel formPanel = new JPanel();
 
+        tablePanel.setLayout(new BorderLayout());
+        tablePanel.add(setTable());
 
-        cp.add((Component) setTable());
+        formPanel.setLayout(new BorderLayout());
+        formPanel.add(setForm());
+
+        cp.add(tablePanel, BorderLayout.WEST);
+        cp.add(formPanel, BorderLayout.EAST);
 
         setSize(1000, 500);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    private BorderLayout setTable() {
-        BorderLayout layout = new BorderLayout();
-
-        String[] tableHeader = {"Name", "AZ", "Type", "Public IP", "Private IP"};
+    private JScrollPane setTable() {
+        String[] tableHeader = {"ID", "Name", "AZ", "Type", "Public IP", "Private IP"};
         String[][] tableContents = new String[0][];
 
-        JTable instanceTable = new JTable(tableContents, tableHeader);
-        JScrollPane instanceTableScrollPane = new JScrollPane(instanceTable);
+        DefaultTableModel instanceTableModel = new DefaultTableModel(tableContents, tableHeader);
 
-        layout.addLayoutComponent(instanceTableScrollPane, BorderLayout.CENTER);
+        instanceTable = new JTable(instanceTableModel);
 
-        return layout;
+        return new JScrollPane(instanceTable);
+    }
+
+    private JPanel setForm() {
+        JPanel formPanel = new JPanel();
+        JButton button = new JButton("OK");
+        JTextField accessKeyTextField = new JTextField(20);
+        JTextField secretAccessKeyTextField = new JTextField(20);
+        JComboBox<String> regionComboBox = new JComboBox<>(region);
+
+        formPanel.setLayout(new GridLayout(4, 2));
+
+        formPanel.add(new JLabel("Access Key "));
+        formPanel.add(accessKeyTextField);
+
+        formPanel.add(new JLabel("Secret Access Key "));
+        formPanel.add(secretAccessKeyTextField);
+
+        formPanel.add(new JLabel("Region "));
+        formPanel.add(regionComboBox);
+
+        button.addActionListener(new EventListener(accessKeyTextField, secretAccessKeyTextField, regionComboBox, instanceTable));
+        formPanel.add(button);
+
+        setSize(500, 300);
+
+        return formPanel;
     }
 }
 
